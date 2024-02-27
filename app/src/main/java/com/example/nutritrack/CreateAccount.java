d1a1d1b22ea0888132361fb64757bc9dda0571bb
pramodh-douglas <134018126+pramodh-douglas@users.noreply.github.com>
2024-02-26 19:42:39 -0800

    added material components to input fields

--- app/src/main/java/com/example/nutritrack/CreateAccount.java
+++ app/src/main/java/com/example/nutritrack/CreateAccount.java
@@ -2,13 +2,28 @@ package com.example.nutritrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.nutritrack.databinding.ActivityCreateAccountBinding;
+import com.example.nutritrack.databinding.ActivityLoginBinding;
 
 public class CreateAccount extends AppCompatActivity {
+
+    ActivityCreateAccountBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateAccount.this, Welcome.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nutritrack/Login.java
+++ app/src/main/java/com/example/nutritrack/Login.java
@@ -2,13 +2,29 @@ package com.example.nutritrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.nutritrack.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
+
+    ActivityLoginBinding loginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(loginBinding.getRoot());
+
+        loginBinding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Welcome.class));
+                finish();
+            }
+        });
     }
 }
